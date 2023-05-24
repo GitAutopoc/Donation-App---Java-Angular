2140c668e5b3391ac32f3b2b724bc722af4d2abc
GitAutopoc <98872904+GitAutopoc@users.noreply.github.com>
2023-05-24 12:39:05 +0530

    Update TestUtils.java

--- Donation App - Java Angular/Donation App - Java Angular/Backend/src/test/java/com/iiht/training/ngo/testutils/TestUtils.java
+++ Donation App - Java Angular/Donation App - Java Angular/Backend/src/test/java/com/iiht/training/ngo/testutils/TestUtils.java
@@ -38,7 +38,7 @@ public class TestUtils {
 
 	public static final String GUID = "6ed39465-d6d3-4ec4-b27d-1dcb870b2992";
 	public static String customData;
-	public static final String URL = "https://yaksha-prod-sbfn.azurewebsites.net/api/YakshaMFAEnqueue?code=jSTWTxtQ8kZgQ5FC0oLgoSgZG7UoU9Asnmxgp6hLLvYId/GW9ccoLw==";
+	public static final String URL = "https://yaksha-uat-sbfn.azurewebsites.net/api/YakshaMFAEnqueue?code=b2Nouy31ANrObyhFMwy13abG7P7w9I5ls1XeZcXVBSqlE8VDwzBCRA==";
 	static {
 		total = 0;
 		passed = 0;
